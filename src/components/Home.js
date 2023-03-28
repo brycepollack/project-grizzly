224b7eaae2dcd51a115fe521c6a509e3e6ee8ea2
Bryce Pollack <bnpollack@ucsd.edu>
2023-03-27 23:08:19 -0700

    Feat: Basic home component

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = ({ isDev }) => {
+  return (
+    <div className="parent-container">
+      <div className="container">
+        Nothing to see here! Just a box
+      </div>
+    </div>
+  )
+}
+
+export default Home
