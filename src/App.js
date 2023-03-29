4237dbff6e0ff4213bd16c65f787f95536824a85
Bryce Pollack <bnpollack@ucsd.edu>
2023-03-28 17:01:15 -0700

    Chore: Updated style sheet

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import { BrowserRouter, Switch, Route, Link, Routes } from "react-router-dom";
 import Home from "./components/Home";
 
 const App = () => {
-  //CORS error causer make sure to swap true/false
-  const isDev = true;
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home isDev={isDev} />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
