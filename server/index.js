9fb134062f8b57f7bac8afed8699a22049a474b1
Bryce Pollack <bpollack@ucsd.edu>
2024-01-11 18:23:48 -0800

    Readded cookie session, removed serverside console log

--- server/index.js
+++ server/index.js
@@ -17,12 +17,19 @@ console.log("Origin: " + ORIGIN_URL);
 // Connect to database
 connectDB();
 
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["lama"],
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
+
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use((req, res, next) => {
-  console.log('Custom middleware');
   next();
 });
 
